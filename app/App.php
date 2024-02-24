03e9c3d907684bb1ddece3cde7c2d1e97d2caf4e
fadrian06 <franyeradriansanchez@gmail.com>
2024-02-24 19:04:12 -0400

    Añadido App::renderPage

--- app/App.php
+++ app/App.php
@@ -7,9 +7,24 @@ use flight\net\Route;
  * @method static UserRepository userRepository()
  */
 class App extends Flight {
-  static function route(string $pattern, callable $callback, bool $pass_route = false, string $alias = ''): Route {
+  static function route(
+    string $pattern,
+    callable $callback,
+    bool $pass_route = false,
+    string $alias = ''
+  ): Route {
     $alias = substr($pattern, strpos($pattern, '/'));
 
     return self::router()->map($pattern, $callback, $pass_route, $alias);
+  }
+
+  static function renderPage(
+    string $page,
+    string $title,
+    array $params,
+    string $layout = 'base'
+  ): void {
+    App::render("pages/$page", $params, 'content');
+    App::render("layouts/$layout", compact('title'));
   }
 }
