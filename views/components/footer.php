e722cf59be0741e002d680530f0e5ec1faa94a4d
fadrian06 <franyeradriansanchez@gmail.com>
2024-03-08 03:38:50 -0400

    Ajustes en el Pie de Página

--- views/components/footer.php
+++ views/components/footer.php
@@ -1,6 +1,6 @@
-<footer class="footer_part p-0 mx-2 mx-md-0 px-md-5 position-relative">
+<footer class="footer_part p-0 mx-3 px-md-5 position-relative">
   <p class="footer_iner text-center mx-0">
-    <?= date('Y') ?> © UPTM - Intregrantes
+    <?= date('Y') ?> © UPTM - Integrantes
     <i class="ti-heart"></i>
     Daniel Mancilla, Franyer Sánchez, Jénifer Lázaro
   </p>
