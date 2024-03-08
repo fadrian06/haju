ead232d7b69767a21837899c6f8cd0805e2ed365
fadrian06 <franyeradriansanchez@gmail.com>
2024-03-08 10:03:37 -0400

    Traducidas pruebas unitarias

--- tests/PDOUserRepositoryTest.php
+++ tests/PDOUserRepositoryTest.php
@@ -57,7 +57,8 @@ class PDOUserRepositoryTest extends TestCase {
     $this->repository->setConnection($connection);
   }
 
-  function test_it_throws_an_exception_when_users_table_is_not_defined(): void {
+  // function test_it_throws_an_exception_when_users_table_is_not_defined(): void {
+  function test_lanza_un_error_cuando_la_tabla_usuarios_no_está_definida(): void {
     $this->repository->setConnection(new Connection(DBConnection::SQLite, ':memory:'));
 
     self::expectException(ConnectionException::class);
@@ -65,11 +66,13 @@ class PDOUserRepositoryTest extends TestCase {
     $this->repository->getAll();
   }
 
-  function test_it_returns_an_empty_array_when_there_are_no_users(): void {
+  // function test_it_returns_an_empty_array_when_there_are_no_users(): void {
+  function test_retorna_una_lista_vacía_cuando_no_hay_usuarios(): void {
     self::assertSame([], $this->repository->getAll());
   }
 
-  function test_it_returns_one_recent_registered_user(): void {
+  // function test_it_returns_one_recent_registered_user(): void {
+  function test_retorna_un_usuario_registrado_recientemente(): void {
     $this->repository->save($this->testUser);
 
     $users = $this->repository->getAll();
@@ -78,7 +81,8 @@ class PDOUserRepositoryTest extends TestCase {
     self::assertEquals($this->testUser, $users[0]);
   }
 
-  function test_it_throws_an_exception_when_connection_is_not_provided(): void {
+  // function test_it_throws_an_exception_when_connection_is_not_provided(): void {
+  function test_lanza_un_error_cuando_la_conexión_no_es_proveída(): void {
     $repository = new PDOUserRepository;
 
     self::expectException(ConnectionException::class);
@@ -94,7 +98,8 @@ class PDOUserRepositoryTest extends TestCase {
     self::assertEquals($this->testUser, $user);
   }
 
-  function test_can_save_multiple_users(): void {
+  // function test_can_save_multiple_users(): void {
+  function test_puede_guardar_múltiples_usuarios(): void {
     $user2 = new User(
       'Franyer',
       'Guillén',
@@ -116,7 +121,8 @@ class PDOUserRepositoryTest extends TestCase {
     self::assertEquals($user2, $users[1]);
   }
 
-  function test_cannot_save_multiple_users_with_the_same_id_card(): void {
+  // function test_cannot_save_multiple_users_with_the_same_id_card(): void {
+  function test_no_puede_guardar_múltiples_usuarios_con_la_misma_cédula(): void {
     $this->repository->save($this->testUser);
 
     self::expectException(DuplicatedIdCardException::class);
@@ -134,7 +140,8 @@ class PDOUserRepositoryTest extends TestCase {
     ));
   }
 
-  function test_cannot_save_multiple_users_with_the_same_names(): void {
+  // function test_cannot_save_multiple_users_with_the_same_names(): void {
+  function test_no_puede_guardar_múltiples_usuarios_con_los_mismos_nombres(): void {
     $this->repository->save($this->testUser);
 
     self::expectException(DuplicatedNamesException::class);
@@ -152,7 +159,8 @@ class PDOUserRepositoryTest extends TestCase {
     ));
   }
 
-  function test_cannot_save_multiple_users_with_the_same_phones(): void {
+  // function test_cannot_save_multiple_users_with_the_same_phones(): void {
+  function test_no_puede_guardar_múltiples_usuarios_con_el_mismo_teléfono(): void {
     $this->repository->save($this->testUser);
 
     self::expectException(DuplicatedPhonesException::class);
@@ -171,7 +179,8 @@ class PDOUserRepositoryTest extends TestCase {
     ));
   }
 
-  function test_cannot_save_multiple_users_with_the_same_emails(): void {
+  // function test_cannot_save_multiple_users_with_the_same_emails(): void {
+  function test_no_puede_guardar_múltiples_usuarios_con_el_mismo_correo(): void {
     $this->repository->save($this->testUser);
 
     self::expectException(DuplicatedEmailsException::class);
@@ -190,7 +199,8 @@ class PDOUserRepositoryTest extends TestCase {
     ));
   }
 
-  function test_cannot_save_multiple_users_with_the_same_avatars(): void {
+  // function test_cannot_save_multiple_users_with_the_same_avatars(): void {
+  function test_no_puede_guardar_múltiples_usuarios_con_el_mismo_avatar(): void {
     $this->repository->save($this->testUser);
 
     self::expectException(DuplicatedAvatarsException::class);
@@ -209,7 +219,8 @@ class PDOUserRepositoryTest extends TestCase {
     ));
   }
 
-  function test_can_update_the_user_password(): void {
+  // function test_can_update_the_user_password(): void {
+  function test_puede_actualizar_la_contraseña_del_usuario(): void {
     $user = clone $this->testUser;
     self::assertTrue($user->checkPassword('test1234'));
 
@@ -224,7 +235,8 @@ class PDOUserRepositoryTest extends TestCase {
     self::assertTrue($this->repository->getByIdCard(28072391)->checkPassword('test1234updated'));
   }
 
-  function test_it_retrieves_a_registered_user_by_id_card(): void {
+  // function test_it_retrieves_a_registered_user_by_id_card(): void {
+  function test_recupera_un_usuario_registrado_por_cédula(): void {
     $this->repository->save($this->testUser);
 
     $user = $this->repository->getByIdCard($this->testUser->idCard);
@@ -235,13 +247,15 @@ class PDOUserRepositoryTest extends TestCase {
     self::assertSame($this->testUser->getPassword(), $user->getPassword());
   }
 
-  function test_it_returns_null_when_an_unexistent_id_card_is_given(): void {
+  // function test_it_returns_null_when_an_unexistent_id_card_is_searched(): void {
+  function test_retorna_vacío_cuando_una_cédula_inexistente_es_buscada(): void {
     $this->repository->save($this->testUser);
 
     self::assertNull($this->repository->getByIdCard($this->testUser->idCard + 1));
   }
 
-  function test_it_retrieves_a_registered_user_by_id(): void {
+  // function test_it_retrieves_a_registered_user_by_id(): void {
+  function test_recupera_un_usuario_registrado_por_id(): void {
     $this->repository->save($this->testUser);
 
     $user = $this->repository->getById($this->testUser->getId());
@@ -252,7 +266,8 @@ class PDOUserRepositoryTest extends TestCase {
     self::assertSame($this->testUser->getPassword(), $user->getPassword());
   }
 
-  function test_it_returns_null_when_an_unexistent_id_is_given(): void {
+  // function test_it_returns_null_when_an_unexistent_id_is_given(): void {
+  function test_retorna_vacío_cuando_un_id_inexistente_es_buscado(): void {
     $this->repository->save($this->testUser);
 
     self::assertNull($this->repository->getById($this->testUser->getId() + 1));
