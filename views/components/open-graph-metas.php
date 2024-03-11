862c75dca58197aa4a7b333ba0a1d12b872306fc
fadrian06 <franyeradriansanchez@gmail.com>
2024-03-10 21:55:31 -0400

    Actualizadas las etiquetas OpenGraph para el nuevo dominio de producción

--- views/components/open-graph-metas.php
+++ views/components/open-graph-metas.php
@@ -2,18 +2,18 @@
 <meta name="description" content="Sistema Automatizado para la Gestión Estadística del Hospital General Municipal José Antonio Uzcátegui">
 
 <!-- Facebook Meta Tags -->
-<meta property="og:url" content="https://faslatam.000webhostapp.com/proyectos/haju">
+<meta property="og:url" content="http://faslatam.000.pe/haju">
 <meta property="og:type" content="website">
 <meta property="og:title" content="HAJU - Sistema de Gestión de Estadísticas">
 <meta property="og:description" content="Sistema Automatizado para la Gestión Estadística del Hospital General Municipal José Antonio Uzcátegui">
-<meta property="og:image" content="https://opengraph.b-cdn.net/production/documents/b9c506b6-548b-4fd2-9799-777d749e3f64.png?token=LuJXFWUTQTXmEQ93dZFE7tzUhARuihaUIiE0LOYUs_U&height=494&width=1020&expires=33244751955">
+<meta property="og:image" content="https://opengraph.b-cdn.net/production/documents/ddfdaf48-209f-4bf6-a83c-1ed5087df678.png?token=vUcv2qwOWrFsJfcIBn3cXfTBOu0uBYyV1jpU1G8Y4zo&height=494&width=1020&expires=33246122052">
 
 <!-- Twitter Meta Tags -->
 <meta name="twitter:card" content="summary_large_image">
-<meta property="twitter:domain" content="faslatam.000webhostapp.com">
-<meta property="twitter:url" content="https://faslatam.000webhostapp.com/proyectos/haju">
+<meta property="twitter:domain" content="faslatam.000.pe">
+<meta property="twitter:url" content="http://faslatam.000.pe/haju">
 <meta name="twitter:title" content="HAJU - Sistema de Gestión de Estadísticas">
 <meta name="twitter:description" content="Sistema Automatizado para la Gestión Estadística del Hospital General Municipal José Antonio Uzcátegui">
-<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/b9c506b6-548b-4fd2-9799-777d749e3f64.png?token=LuJXFWUTQTXmEQ93dZFE7tzUhARuihaUIiE0LOYUs_U&height=494&width=1020&expires=33244751955">
+<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/ddfdaf48-209f-4bf6-a83c-1ed5087df678.png?token=vUcv2qwOWrFsJfcIBn3cXfTBOu0uBYyV1jpU1G8Y4zo&height=494&width=1020&expires=33246122052">
 
 <!-- Meta Tags Generated via https://www.opengraph.xyz -->
