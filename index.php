29472419dc2228e88c055e0b23acfad440530c12
fadrian06 <franyeradriansanchez@gmail.com>
2024-10-05 17:32:25 -0400

    Renombrado config.php a configurations.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/app/config.php';
+require_once __DIR__ . '/app/configurations.php';
 require_once __DIR__ . '/app/routes/all.php';
 
 App::start();
