b6ddf067f46438cbebdd0b8752709afd51ad4788
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-05 13:05:02 -0400

    Inyección de dependencias en AuthenticationMiddleware

--- app/Middlewares/AuthenticationMiddleware.php
+++ app/Middlewares/AuthenticationMiddleware.php
@@ -4,19 +4,29 @@ declare(strict_types=1);
 
 namespace App\Middlewares;
 
-use App;
+use App\Repositories\Domain\UserRepository;
+use Flight;
+use flight\template\View;
+use Leaf\Http\Session;
 
 final readonly class AuthenticationMiddleware {
-  public static function before(): void {
-    if (
-      !App::session()->get('userId')
-      || !$user = App::userRepository()->getById((int) App::session()->get('userId'))
-    ) {
-      App::redirect('/salir');
+  public function __construct(
+    private Session $session,
+    private UserRepository $userRepository,
+    private View $view,
+  ) {
+  }
 
-      exit;
+  public function before(): void {
+    if (
+      !$this->session->get('userId')
+      || !$user = $this->userRepository->getById((int) $this->session->get('userId'))
+    ) {
+      Flight::redirect('/salir');
+
+      return;
     }
 
-    App::view()->set('user', $user);
+    $this->view->set('user', $user);
   }
 }
