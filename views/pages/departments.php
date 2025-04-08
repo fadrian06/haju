f48b8dc8c7d37459a45ca714256733232317a32f
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-07 23:31:45 -0400

    Eliminado el formulario de aperturar departamento

--- views/pages/departments.php
+++ views/pages/departments.php
@@ -16,10 +16,6 @@ use App\Models\User;
 
 <section class="mb-4 d-sm-flex px-0 align-items-center justify-content-between">
   <h2>Departamentos</h2>
-  <!-- <a data-bs-toggle="modal" href="#registrar" class="btn btn-primary rounded-pill d-flex align-items-center">
-    <i class="px-2 ti-plus"></i>
-    <span class="px-2">Añadir departamento</span>
-  </a> -->
 </section>
 
 <?php if ($departments !== []) : ?>
@@ -39,7 +35,11 @@ use App\Models\User;
             </div>
           </div>
           <picture class="p-3">
-            <img class="img-fluid" src="<?= urldecode($department->iconFilePath->asString()) ?>" style="height: 130px; object-fit: contain;" title="<?= $department->name ?>" />
+            <img
+              class="img-fluid"
+              src="<?= urldecode($department->iconFilePath->asString()) ?>"
+              style="height: 130px; object-fit: contain;"
+              title="<?= $department->name ?>" />
           </picture>
           <span class="custom-badge status-<?= $department->isActive() ? 'green' : 'red' ?> mx-4 mb-2">
             <?= $department->isActive() ? 'Activo' : 'Inactivo' ?>
@@ -53,58 +53,3 @@ use App\Models\User;
     <?php endforeach ?>
   </ul>
 <?php endif ?>
-
-<!-- <div class="modal fade" id="registrar">
-  <div class="modal-dialog">
-    <form action="./departamentos#registrar" enctype="multipart/form-data" class="modal-content" method="post">
-      <header class="modal-header">
-        <h3 class="modal-title fs-5">Añadir departamento</h3>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-      </header>
-      <section class="modal-body">
-        <?php
-
-        Flight::render('components/input-group', [
-          'name' => 'name',
-          'placeholder' => 'Nombre del departamento',
-          'cols' => 12
-        ]);
-
-        Flight::render('components/input-group', [
-          'variant' => 'file',
-          'name' => 'department_icon',
-          'placeholder' => 'Icono'
-        ]);
-
-        Flight::render('components/input-group', [
-          'variant' => 'checkbox',
-          'name' => 'belongs_to_external_consultation',
-          'placeholder' => 'Pertenece a Consulta Externa'
-        ]);
-
-        Flight::render('components/input-group', [
-          'variant' => 'checkbox',
-          'name' => 'is_active',
-          'placeholder' => 'Estado <small>(activo/inactivo)</small>',
-          'checked' => true
-        ]);
-
-        ?>
-      </section>
-      <footer class="modal-footer">
-        <button class="btn btn-primary">Añadir</button>
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-          Cancelar
-        </button>
-      </footer>
-    </form>
-  </div>
-</div> -->
-
-<script>
-  document.addEventListener('DOMContentLoaded', () => {
-    if (location.href.endsWith('#registrar')) {
-      new bootstrap.Modal('#registrar').show()
-    }
-  })
-</script>
