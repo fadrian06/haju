c41bac9561b2da9879f06fcf33c6d8926f5d90d6
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-07 23:33:48 -0400

    Eliminada el acerca de en el perfil del usuario

--- views/pages/profile.php
+++ views/pages/profile.php
@@ -59,9 +59,6 @@ use App\ValueObjects\Appointment;
   <ul class="nav nav-tabs row mx-0 px-0 mt-4">
     <li class="nav-item col px-0">
       <div class="row mx-0">
-        <!-- <button class="nav-link col-sm px-0" data-bs-toggle="tab" data-bs-target="#about-cont">
-          Acerca de
-        </button> -->
         <button class="nav-link col-sm px-0 active" data-bs-toggle="tab" data-bs-target="#seguridad">
           Seguridad
         </button>
@@ -70,41 +67,6 @@ use App\ValueObjects\Appointment;
   </ul>
 </article>
 <section class="tab-content mt-4 px-0">
-  <!-- <article class="tab-pane fade" id="about-cont">
-    <div class="row mx-0">
-      <article class="col-md px-0 pe-md-2">
-        <div class="white_box">
-          <h3>Información de Educación</h3>
-          <ul class="timeline mt-4">
-            <?php foreach (range(1, 1) as $_) : ?>
-              <li class="timeline-item">
-                <a href="#" target="_blank">
-                  <strong class="text-secondary">International College of Medical Science (UG)</strong>
-                  <span class="text-black-50 fw-semibold">MBBS</span>
-                  <time class="small text-black-50">2001 - 2003</time>
-                </a>
-              </li>
-            <?php endforeach ?>
-          </ul>
-        </div>
-      </article>
-      <article class="col-md px-0 ps-md-2 mt-4 mt-md-0">
-        <div class="white_box">
-          <h3>Experience</h3>
-          <ul class="timeline mt-4">
-            <?php foreach (range(1, 1) as $_) : ?>
-              <li class="timeline-item">
-                <a href="#" target="_blank">
-                  <strong class="text-secondary">Consultant Gynecologist</strong>
-                  <time class="small text-black-50">Jan 2014 - Present (4 years 8 months)</time>
-                </a>
-              </li>
-            <?php endforeach ?>
-          </ul>
-        </div>
-      </article>
-    </div>
-  </article> -->
   <section class="tab-pane fade show active row px-2" id="seguridad">
     <form method="post" action="./perfil#seguridad" class="col-md white_box">
       <h3 class="mb-4">Cambiar contraseña</h3>
