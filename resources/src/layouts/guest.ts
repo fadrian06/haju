5576a480a4a7546c22d3b0763181de35fecdcd30
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-14 17:22:37 -0400

    npm run format

--- resources/src/layouts/guest.ts
+++ resources/src/layouts/guest.ts
@@ -1,25 +1,25 @@
-import 'alpinejs/dist/cdn.min'
-import 'bootstrap/js/dist/carousel'
-import 'bootstrap/js/dist/dropdown'
-import 'bootstrap/js/dist/modal'
-import Tooltip from 'bootstrap/js/dist/tooltip'
-import Swal from 'sweetalert2'
-import '../styles/customizations.scss'
+import "alpinejs/dist/cdn.min";
+import "bootstrap/js/dist/carousel";
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/modal";
+import Tooltip from "bootstrap/js/dist/tooltip";
+import Swal from "sweetalert2";
+import "../styles/customizations.scss";
 
-const tooltipElements = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipElements = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 
 for (const element of Array.from(tooltipElements)) {
-  new Tooltip(element)
+  new Tooltip(element);
 }
 
 globalThis.customSwal = Swal.mixin({
   showCloseButton: true,
   customClass: {
-    confirmButton: 'btn btn-primary mx-2',
-    denyButton: 'btn btn-danger mx-2',
-    popup: 'bg-white border',
-    title: 'bg-white',
+    confirmButton: "btn btn-primary mx-2",
+    denyButton: "btn btn-danger mx-2",
+    popup: "bg-white border",
+    title: "bg-white",
   },
   buttonsStyling: false,
   keydownListenerCapture: true,
-})
+});
