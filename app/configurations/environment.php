109a5ea18ae44642695e11bd013c151103caea6b
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-17 19:27:12 -0400

    Simplificada la carga de variables de entorno

--- app/configurations/environment.php
+++ app/configurations/environment.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 use App\Enums\DBDriver;
 use Symfony\Component\Dotenv\Dotenv;
 
-(new Dotenv)->load(
-  __DIR__ . '/../../.env.dist',
-  __DIR__ . '/../../.env'
-);
+$dotenv = new Dotenv;
+$dotenv->load(__DIR__ . '/../../.env.dist', __DIR__ . '/../../.env');
 
 $_ENV['DB_CONNECTION'] = DBDriver::from($_ENV['DB_CONNECTION']);
+$_ENV['DB_PORT'] = intval($_ENV['DB_PORT']);
+$_ENV['DB_USERNAME'] = $_ENV['DB_USERNAME'] ?: null;
+$_ENV['DB_PASSWORD'] = $_ENV['DB_PASSWORD'] ?: null;
