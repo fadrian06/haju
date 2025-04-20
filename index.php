86c1a1c6791c145949e881a114a5f68363384d95
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-20 13:06:16 -0400

    Movido las constantes a index.php

--- index.php
+++ index.php
@@ -8,7 +8,22 @@ try {
   error_reporting(E_ALL);
 
   require_once __DIR__ . '/vendor/autoload.php';
-  require_once __DIR__ . '/app/constants.php';
+
+  /**
+   * - `''`: with _composer serve_ -> _localhost:61001_
+   * - `'/haju'`: with xampp -> _localhost/haju_
+   * - `'/faslatam.42web.io/htdocs/haju'`: hosting uri
+   */
+  define('BASE_URI', str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']));
+
+  $_SERVER['HTTP_HOST'] ??= 'localhost:61001';
+
+  /** `http://localhost:61001` */
+  define(
+    'BASE_URL',
+    Flight::request()->scheme . '://' . $_SERVER['HTTP_HOST'] . BASE_URI
+  );
+
   require_once __DIR__ . '/app/configurations/environment.php';
   require_once __DIR__ . '/app/configurations/container.php';
   require_once __DIR__ . '/app/configurations/flight.php';
