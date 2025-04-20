a67d6cd3c181bfc82f02e5748700eec4cd566cc6
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-20 12:28:12 -0400

    Mostrar errores siempre

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 
 declare(strict_types=1);
 
-error_reporting(E_ALL);
+try {
+  error_reporting(E_ALL);
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/app/constants.php';
-require_once __DIR__ . '/app/configurations/environment.php';
-require_once __DIR__ . '/app/configurations/container.php';
-require_once __DIR__ . '/app/configurations/flight.php';
-require_once __DIR__ . '/app/routes/web.php';
-require_once __DIR__ . '/app/routes/api.php';
+  require_once __DIR__ . '/vendor/autoload.php';
+  require_once __DIR__ . '/app/constants.php';
+  require_once __DIR__ . '/app/configurations/environment.php';
+  require_once __DIR__ . '/app/configurations/container.php';
+  require_once __DIR__ . '/app/configurations/flight.php';
+  require_once __DIR__ . '/app/routes/web.php';
+  require_once __DIR__ . '/app/routes/api.php';
 
-date_default_timezone_set($_ENV['TIMEZONE']);
+  date_default_timezone_set($_ENV['TIMEZONE']);
 
-Flight::start();
+  Flight::start();
+} catch (Throwable $error) {
+  exit("<pre>$error</pre>");
+}
