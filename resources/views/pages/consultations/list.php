532a8a57d8471026bce3d160fe419b64cc363373
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-24 21:52:26 -0400

    Soporte modo oscuro en el listado de consultas

--- resources/views/pages/consultations/list.php
+++ resources/views/pages/consultations/list.php
@@ -13,13 +13,13 @@ use HAJU\Enums\DateRange;
 
 ?>
 
-<section class="mb-4 d-inline-flex align-items-center justify-content-between">
+<section class="container mb-4 d-flex align-items-center justify-content-between">
   <h2>Consultas</h2>
   <?php if (!$department->isStatistics()) : ?>
     <a
       href="./consultas/registrar"
-      class="btn btn-primary rounded-pill d-flex align-items-center">
-      <i class="px-2 ti-plus"></i>
+      class="btn btn-primary d-flex align-items-center">
+      <i class="px-2 fa fa-plus"></i>
       <span class="px-2">Registrar consulta</span>
     </a>
   <?php endif ?>
@@ -45,37 +45,30 @@ use HAJU\Enums\DateRange;
           const startDate = new Date(this.startDate);
           const consultationDate = new Date(consultation.registeredDateImperialFormat);
 
-          match = match || consultationDate >= startDate;
+          match = match && consultationDate >= startDate;
         }
 
         if (this.endDate) {
           const endDate = new Date(this.endDate);
           const consultationDate = new Date(consultation.registeredDateImperialFormat);
 
-          match = match || consultationDate <= endDate;
+          match = match && consultationDate <= endDate;
         }
 
         return match;
       });
     },
   }'
-  class="white_box QA_section">
-  <header class="list_header serach_field-area2 w-100 mb-3">
-    <form @submit.prevent class="search_inner w-100">
-      <input
-        type="search"
-        placeholder="Buscar por paciente..."
-        x-model="patientName" />
-      <button>
-        <i class="ti-search fs-2"></i>
-      </button>
-    </form>
-  </header>
+  class="card card-body">
+  <?php Flight::render('components/inputs/input', [
+    'type' => 'search',
+    'model' => 'patientName',
+    'label' => 'Buscar por paciente...'
+  ]) ?>
 
   <h3 class="my-4 fs-2">Filtrar por</h3>
-  <div class="list-group mb-3">
-    <div
-      class="list-group-item d-flex justify-content-between align-items-center">
+  <div class="list-group list-group-flush mb-3">
+    <div class="list-group-item d-flex justify-content-between align-items-center">
       <select x-model="type" class="form-select">
         <option value="" selected>Tipo de consulta</option>
         <option value="P">P: Primera vez</option>
@@ -111,7 +104,7 @@ use HAJU\Enums\DateRange;
     </div>
   </div>
 
-  <div class="QA_table table-responsive">
+  <div class="table-responsive">
     <table class="table text-center">
       <thead>
         <tr>
