17bdf42b24e0dcf8944cfdb78260e797a5d223fc
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-24 21:41:35 -0400

    Soporte a modo oscuro en el perfil de paciente

--- resources/views/pages/patients/info.php
+++ resources/views/pages/patients/info.php
@@ -33,104 +33,106 @@ $patientAvatar = @file_get_contents($avatarUrl)
   const causesCounters = JSON.parse('<?= json_encode(array_values($causes)) ?>')
 </script>
 
-<section class="mb-4 d-flex px-0 align-items-center justify-content-between">
+<section class="container mb-4 d-flex px-0 align-items-center justify-content-between">
   <h2 class="m-0">Detalles del paciente</h2>
 </section>
-<article class="white_box px-0 pb-0 row align-items-center">
-  <div class="profile-info col-md">
-    <header class="profile-info__main py-2 py-md-0 my-2 my-md-0">
-      <div class="row">
-        <picture class="col-md-4 d-flex align-items-center justify-content-center mb-2 mb-md-0">
-          <img
-            style="max-height: 150px"
-            class="img-fluid rounded-circle"
-            src="<?= $patientAvatar ?>" />
-        </picture>
-        <div class="col-md d-flex flex-column">
-          <h4 class="h3">Paciente: <?= $patient->getFullName() ?></h4>
-          <strong>Cédula: V-<?= $patient->idCard ?></strong>
-          <small class="text">Genero: <?= $patient->gender->value ?></small>
-          <small class="text-muted">Registrado el: <?= $patient->registeredDate ?></small>
-        </div>
-      </div>
-      <div class="row pt-3 mt-3 justify-content-end align-items-center" style="border-top: 2px dashed #ccc">
-        <picture class="col-4 d-none d-md-block text-center">
-          <img
-            src="<?= urldecode($patient->registeredBy->profileImagePath->asString()) ?>"
-            class="img-fluid rounded-circle" />
-        </picture>
-        <div class="col">
-          <h5>Registrado por:</h5>
-          <div class="col">
-            <h6><?= $patient->registeredBy->getFullName() ?></h6>
-            <span>V-<?= $patient->registeredBy->idCard ?></span>
-            <span class="d-block"><?= $patient->registeredBy->getParsedAppointment() ?></span>
-            <small class="text-muted"><?= $patient->registeredBy->phone ?></small>
-            <small class="text-muted">
-              <?= $patient->registeredBy->email->asString() ?>
-            </small>
+<div class="container card card-body">
+  <article class="row align-items-center">
+    <div class="profile-info col-md">
+      <header class="profile-info__main py-2 py-md-0 my-2 my-md-0">
+        <div class="row">
+          <picture class="col-md-4 d-flex align-items-center justify-content-center mb-2 mb-md-0">
+            <img
+              style="max-height: 150px"
+              class="img-fluid rounded-circle"
+              src="<?= $patientAvatar ?>" />
+          </picture>
+          <div class="col-md d-flex flex-column">
+            <h4 class="h3">Paciente: <?= $patient->getFullName() ?></h4>
+            <strong>Cédula: V-<?= $patient->idCard ?></strong>
+            <small class="text">Genero: <?= $patient->gender->value ?></small>
+            <small class="text-muted">Registrado el: <?= $patient->registeredDate ?></small>
           </div>
         </div>
-      </div>
-    </header>
-  </div>
-  <ul class="profile-info__secondary col-md-6">
-    <li>
-      <strong>Primer nombre:</strong>
-      <span><?= $patient->firstName ?></span>
-    </li>
-    <li>
-      <strong>Segundo nombre:</strong>
-      <?php if ($patient->secondName) : ?>
-        <span><?= $patient->secondName ?></span>
-      <?php else : ?>
-        <mark class="text-muted">No establecido</mark>
-      <?php endif ?>
-    </li>
-    <li>
-      <strong>Primer apellido:</strong>
-      <span><?= $patient->firstLastName ?></span>
-    </li>
-    <li>
-      <strong>Segundo apellido:</strong>
-      <?php if ($patient->secondLastName) : ?>
-        <span><?= $patient->secondLastName ?></span>
-      <?php else : ?>
-        <mark class="text-muted">No establecido</mark>
-      <?php endif ?>
-    </li>
-    <li>
-      <strong>Fecha de nacimiento:</strong>
-      <span><?= $patient->birthDate ?></span>
-    </li>
-  </ul>
-  <h3 class="mt-4 mb-0 py-3 text-center active border">
-    <strong>Historial médico</strong>
-  </h3>
-  <ul class="nav nav-tabs row mx-0 px-0">
-    <li class="nav-item col px-0">
-      <div class="row mx-0">
-        <button class="nav-link col-sm px-0 active" data-bs-toggle="tab" data-bs-target="#visitas">
-          Visitas médicas
-        </button>
-        <button class="nav-link col-sm px-0" data-bs-toggle="tab" data-bs-target="#hospitalizaciones">
-          Hospitalizaciones
-        </button>
-        <!-- <button class="nav-link col-sm px-0" data-bs-toggle="tab" data-bs-target="#">
-          Antecedentes médicos
-        </button> -->
-        <!-- <button class="nav-link col-sm px-0" data-bs-toggle="tab" data-bs-target="#">
-          Tratamientos
-        </button> -->
-      </div>
-    </li>
-  </ul>
-</article>
+        <div class="row pt-3 mt-3 justify-content-end align-items-center" style="border-top: 2px dashed #ccc">
+          <picture class="col-4 d-none d-md-block text-center">
+            <img
+              src="<?= urldecode($patient->registeredBy->profileImagePath->asString()) ?>"
+              class="img-fluid rounded-circle" />
+          </picture>
+          <div class="col">
+            <h5>Registrado por:</h5>
+            <div class="col">
+              <h6><?= $patient->registeredBy->getFullName() ?></h6>
+              <span>V-<?= $patient->registeredBy->idCard ?></span>
+              <span class="d-block"><?= $patient->registeredBy->getParsedAppointment() ?></span>
+              <small class="text-muted"><?= $patient->registeredBy->phone ?></small>
+              <small class="text-muted">
+                <?= $patient->registeredBy->email->asString() ?>
+              </small>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+    <ul class="profile-info__secondary col-md-6">
+      <li>
+        <strong>Primer nombre:</strong>
+        <span><?= $patient->firstName ?></span>
+      </li>
+      <li>
+        <strong>Segundo nombre:</strong>
+        <?php if ($patient->secondName) : ?>
+          <span><?= $patient->secondName ?></span>
+        <?php else : ?>
+          <mark class="text-muted">No establecido</mark>
+        <?php endif ?>
+      </li>
+      <li>
+        <strong>Primer apellido:</strong>
+        <span><?= $patient->firstLastName ?></span>
+      </li>
+      <li>
+        <strong>Segundo apellido:</strong>
+        <?php if ($patient->secondLastName) : ?>
+          <span><?= $patient->secondLastName ?></span>
+        <?php else : ?>
+          <mark class="text-muted">No establecido</mark>
+        <?php endif ?>
+      </li>
+      <li>
+        <strong>Fecha de nacimiento:</strong>
+        <span><?= $patient->birthDate ?></span>
+      </li>
+    </ul>
+    <h3 class="mt-4 mb-0 py-3 text-center active border">
+      <strong>Historial médico</strong>
+    </h3>
+    <ul class="nav nav-tabs row mx-0 px-0">
+      <li class="nav-item col px-0">
+        <div class="row mx-0">
+          <button class="nav-link col-sm px-0 active" data-bs-toggle="tab" data-bs-target="#visitas">
+            Visitas médicas
+          </button>
+          <button class="nav-link col-sm px-0" data-bs-toggle="tab" data-bs-target="#hospitalizaciones">
+            Hospitalizaciones
+          </button>
+          <!-- <button class="nav-link col-sm px-0" data-bs-toggle="tab" data-bs-target="#">
+            Antecedentes médicos
+          </button> -->
+          <!-- <button class="nav-link col-sm px-0" data-bs-toggle="tab" data-bs-target="#">
+            Tratamientos
+          </button> -->
+        </div>
+      </li>
+    </ul>
+  </article>
+</div>
 <section class="tab-content mt-4 px-0">
   <article class="tab-pane fade show active" id="visitas">
     <div class="row mx-0">
       <article class="col-md px-0 pe-md-2">
-        <div class="white_box">
+        <div class="card card-body">
           <h3>Visitas médicas <sub>(de la más reciente a la más antigua)</sub></h3>
           <ul class="timeline mt-4">
             <?php if (!$patient->hasConsultations()) : ?>
@@ -140,11 +142,20 @@ $patientAvatar = @file_get_contents($avatarUrl)
 
             <?php foreach ($patient->getConsultation() as $consultation) : ?>
               <li class="timeline-item">
-                <a target="_blank" data-bs-toggle="tooltip" title="<?= $consultation->cause->category->extendedName ?? $consultation->cause->category->shortName ?>">
-                  <strong class="text-secondary"><?= $consultation->cause->getFullName() ?></strong>
-                  <span class="text-black-50 fw-semibold"><?= $consultation->type->getDescription() ?></span>
-                  <time class="small text-black-50"><?= $consultation->registeredDate ?></time>
-                  <small class="small text-black-50">
+                <a
+                  target="_blank"
+                  data-bs-toggle="tooltip"
+                  title="<?= $consultation->cause->category->extendedName ?? $consultation->cause->category->shortName ?>">
+                  <strong>
+                    <?= $consultation->cause->getFullName() ?>
+                  </strong>
+                  <span>
+                    <?= $consultation->type->getDescription() ?>
+                  </span>
+                  <time>
+                    <?= $consultation->registeredDate ?>
+                  </time>
+                  <small>
                     Atendido por: <?= $consultation->doctor->getFullName() ?>
                   </small>
                 </a>
@@ -154,7 +165,7 @@ $patientAvatar = @file_get_contents($avatarUrl)
         </div>
       </article>
       <article class="col-md px-0 ps-md-2 mt-4 mt-md-0">
-        <div class="white_box">
+        <div class="card card-body">
           <h3>Frecuentes</h3>
           <canvas id="frecuent-consultation-causes" style="width: 100%"></canvas>
         </div>
@@ -165,16 +176,18 @@ $patientAvatar = @file_get_contents($avatarUrl)
   <article class="tab-pane fade" id="hospitalizaciones">
     <div class="row mx-0">
       <article class="col-md-6 px-0 pe-md-2">
-        <div class="white_box">
+        <div class="card card-body">
           <h3>Histórico</h3>
           <ul class="timeline mt-4">
             <?php foreach ($patient->getHospitalization() as $hospitalization) : ?>
               <li class="timeline-item">
-                <strong class="text-secondary">Hospitalización #<?= $hospitalization->id ?></strong>
-                <time class="small text-black-50"><?= $hospitalization->registeredDate ?></time>
-                <span class="text-black-50 fw-semibold badge text-bg-<?= $hospitalization->isFinished() ? 'success' : 'danger' ?>">
-                  <?= $hospitalization->isFinished() ? 'Finalizada' : 'No finalizada' ?>
-                </span>
+                <strong class="d-flex align-items-center justify-content-between">
+                  Hospitalización #<?= $hospitalization->id ?>
+                  <span class="badge text-bg-<?= $hospitalization->isFinished() ? 'success' : 'danger' ?>">
+                    <?= $hospitalization->isFinished() ? 'Finalizada' : 'No finalizada' ?>
+                  </span>
+                </strong>
+                <time><?= $hospitalization->registeredDate ?></time>
                 <?php if (!$hospitalization->isFinished()) : ?>
                   <a
                     class="btn btn-primary w-100"
