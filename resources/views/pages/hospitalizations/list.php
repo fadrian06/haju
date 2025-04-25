e3f65545d3d4caa6f29dbf1224d60a5f44d9a538
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-24 21:40:41 -0400

    Correcciones en el listado de hospitalizaciones

--- resources/views/pages/hospitalizations/list.php
+++ resources/views/pages/hospitalizations/list.php
@@ -11,13 +11,13 @@ use HAJU\Enums\DateRange;
 
 ?>
 
-<section class="mb-4 d-inline-flex align-items-center justify-content-between">
+<section class="container mb-4 d-flex align-items-center justify-content-between">
   <h2>Hospitalizaciones</h2>
   <?php if (!$department->isHospitalization()) : ?>
     <a
       href="./hospitalizaciones/registrar"
-      class="btn btn-primary rounded-pill d-flex align-items-center">
-      <i class="px-2 ti-plus"></i>
+      class="btn btn-primary d-flex align-items-center">
+      <i class="px-2 fa fa-plus"></i>
       <span class="px-2">Registrar hospitalización</span>
     </a>
   <?php endif ?>
@@ -35,27 +35,21 @@ use HAJU\Enums\DateRange;
 
         if (this.startDate) {
           const startDate = new Date(this.startDate);
-          const hospitalizationDate = new Date(hospitalization.registeredDateImperialFormat);
+          const hospitalizationDate = new Date(hospitalization.admissionDateImperialFormat);
 
-          match = match || hospitalizationDate >= startDate;
+          match = match && hospitalizationDate >= startDate;
         }
 
         return match;
       });
     },
   }'
-  class="white_box QA_section">
-  <header class="list_header serach_field-area2 w-100 mb-3">
-    <form @submit.prevent class="search_inner w-100">
-      <input
-        type="search"
-        placeholder="Buscar por paciente..."
-        x-model="patientName" />
-      <button>
-        <i class="ti-search fs-2"></i>
-      </button>
-    </form>
-  </header>
+  class="container card card-body">
+  <?php Flight::render('components/inputs/input', [
+    'type' => 'search',
+    'model' => 'patientName',
+    'label' => 'Buscar por paciente...'
+  ]) ?>
 
   <h3 class="my-4 fs-2">Filtrar por</h3>
   <div class="list-group mb-3">
@@ -84,6 +78,7 @@ use HAJU\Enums\DateRange;
     <table class="table text-center">
       <thead>
         <tr>
+          <th></th>
           <th>Fecha de ingreso</th>
           <th>Paciente</th>
           <th>Departamento de ingreso</th>
@@ -95,6 +90,13 @@ use HAJU\Enums\DateRange;
           x-for="hospitalization in (patientName || startDate) ? filteredHospitalizations : allHospitalizations"
           x-key="hospitalization.id">
           <tr>
+            <td>
+              <span
+                class="badge"
+                :class="`text-bg-${hospitalization.isFinished ? 'success' : 'danger'}`"
+                x-text="hospitalization.isFinished ? 'Finalizada' : 'No finalizada'">
+              </span>
+            </td>
             <td x-text="hospitalization.admissionDate"></td>
             <td>
               <a
