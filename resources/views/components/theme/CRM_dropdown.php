43c24ff01e4383ca625760209e04c4391f85b689
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-25 22:54:17 -0400

    Añadido el primer componente del tema CRM_dropdown

---
+++ resources/views/components/theme/CRM_dropdown.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+
+<div class="dropdown d-inline-block CRM_dropdown">
+  <button
+    class="btn btn-secondary dropdown-toggle"
+    type="button"
+    data-bs-toggle="dropdown">
+    CRM_dropdown button
+  </button>
+  <ul class="dropdown-menu">
+    <li>
+      <a class="dropdown-item">Action</a>
+    </li>
+    <li>
+      <a class="dropdown-item">Another action</a>
+    </li>
+    <li>
+      <a class="dropdown-item">Something else here</a>
+    </li>
+  </ul>
+</div>
