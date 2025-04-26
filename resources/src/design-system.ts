555d3ec8d909b6ea28e85cd5ecb4a18073ef4ca8
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-25 22:52:20 -0400

    Reducida la carga de fontawesome en el design system

--- resources/src/design-system.ts
+++ resources/src/design-system.ts
@@ -1,6 +1,8 @@
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/solid.min.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./theme/index.css";
 
 import "alpinejs/dist/cdn.min";
-import "bootstrap/js/dist/dropdown";
+import "bootstrap/dist/js/bootstrap";
+import "./setups/bootstrap-tooltips";
