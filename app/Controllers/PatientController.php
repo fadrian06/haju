9d95d87c8fa03c8c87e4cea21d950d671a54b9d2
fadrian06 <franyeradriansanchez@gmail.com>
2025-06-21 23:29:29 -0400

    Arreglado el error de tipos en la propiedad idCard al registrar paciente

--- app/Controllers/PatientController.php
+++ app/Controllers/PatientController.php
@@ -192,7 +192,7 @@ final readonly class PatientController extends Controller
         $this->data['second_last_name'],
         Date::from($this->data['birth_date'], '-'),
         Gender::from($this->data['gender']),
-        $this->data['id_card'],
+        intval($this->data['id_card']),
         $this->loggedUser
       );
 
